c6b7695a4b94fa5f73e69b301bcce5b453e595eb
khdaragon1102 <khdragon1102@gmail.com>
2023-06-01 14:37:11 +0900

    start

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);;
         Controller controller = new Controller(sc);
         while(true){
             controller.selectMode();
